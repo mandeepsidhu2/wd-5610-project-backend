e81fe13f1e4ed42c5796581223becb93ec2fa8bd
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-06-22 21:29:26 -0400

    code added, thunks added, refresh issue solved

--- database/mongo.js
+++ database/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 function dbConnect(){
-    
+    console.log(process.env.MONGODB_URI)
     mongoose.connect(process.env.MONGODB_URI )
     .then(() => console.log('Connected to Mongo!'))
     .catch(()=>console.log('Not Connected to Mongo!'));
